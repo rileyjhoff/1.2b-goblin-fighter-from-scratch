63187bf6249d0431aec7fe703a371859e80b0468
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-17 12:37:30 -0700

    initialized state

--- app.js
+++ app.js
@@ -8,6 +8,33 @@ const goblinListEl = document.querySelector('#goblins-container');
 
 
 // let state
+let defeatedGoblinCount = 0;
+let player = {
+    hp: 10,
+    level: 1,
+    strength: 1,
+    agility: 1,
+    accuracy: 2
+};
+
+let goblins = [
+    {
+        name: 'Hogger',
+        hp: Math.ceil(Math.random() * (4)),
+        level: 1,
+        strength: 1,
+        agility: 1,
+        accuracy: 1
+    },
+    {
+        name: 'Hogger Jr',
+        hp: Math.ceil(Math.random() * (4)),
+        level: 1,
+        strength: 1,
+        agility: 1,
+        accuracy: 1
+    }
+];
 
 // set event listeners 
     // get user input
